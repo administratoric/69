8c3da41c4de6b07f8aaa7bc140c09159af0f35bf
administratoric <149297386+administratoric@users.noreply.github.com>
2026-01-03 23:01:43 +0800

    这是罗马文转换的注释版
    以及考虑了不足之处

--- 罗马文转数字.cpp
+++ 罗马文转数字.cpp
@@ -14,7 +14,7 @@ int fun(char c)
 	case 'M':return 1000;
 	default:return 0;
 	}
-}
+}//使用switch语句进行罗马数字到阿拉伯数字的转换
 
 int main() {
 	char s[15];
@@ -24,12 +24,14 @@ int main() {
 	for (int i = 0; i < n; i++)
 	{
 		int zuo = fun(s[i]);
-		int you = (i + 1 < n) ? fun(s[i + 1]) : 0;
+		int you = (i + 1 < n) ? fun(s[i + 1]) : 0;//条件判断：是否还有下一位罗马数字，有则预加载下一位罗马数字对应的阿拉伯数字
 		if (zuo < you)
-			sum -= zuo;
+			sum -= zuo;//条件判断：如果本位数字小于了下一位数字，则执行减法操作
 		else
-			sum += zuo;
+			sum += zuo;//否则正常执行加法操作
 	}
-	printf("%d", sum);
+	printf("%d", sum);//输出转换结果
 	return 0;
 }
+//其实该方法有不完善的地方，因为罗马数字的特殊运算法则，I（1）只能用于V（5）、X（10）两侧，X只能用于L（50）、C（100）两侧，以此类推
+//意思就是，不会出现IL、IC这种表达，但该算法没有写对应的判断语句，因此请注意
